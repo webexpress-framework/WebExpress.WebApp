760c466e9b8b7e1358ddc81660a944e9b3c7a3ed
Rene Schwarzer <rene_schwarzer@hotmail.de>
2026-07-13 16:07:23 +0200

    feat: add fragments

--- src/WebExpress.WebApp.Test/WebFragment/UnitTestFragmentManager.cs
+++ src/WebExpress.WebApp.Test/WebFragment/UnitTestFragmentManager.cs
@@ -260,6 +260,78 @@ namespace WebExpress.WebApp.Test.WebFragment
         public void GetFragments_RestWorkflow_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataWorkflow), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldataworkflow"" class=""wx-webapp-workflow-editor""></div>");
 
         /// <summary>
+        /// Test the get fragments function of the fragment manager for the comment control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataComment_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataComment), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatacomment"" class=""wx-webapp-comment""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the gantt control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataGantt_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataGantt), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatagantt"" class=""wx-webapp-gantt""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the permission control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataPermission_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataPermission), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatapermission"" class=""wx-webapp-permission""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the scrum backlog control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataScrumBacklog_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataScrumBacklog), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatascrumbacklog"" class=""wx-webapp-scrum-backlog""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the scrum sprint control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataScrumSprint_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataScrumSprint), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatascrumsprint"" class=""wx-webapp-scrum-sprint""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the scrum team control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataScrumTeam_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataScrumTeam), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatascrumteam"" class=""wx-webapp-scrum-team""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the scrum velocity control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataScrumVelocity_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataScrumVelocity), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatascrumvelocity"" class=""wx-webapp-scrum-velocity""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the sidebar control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_DataSidebar_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlDataSidebar), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontroldatasidebar"" class=""wx-webapp-sidebar""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the system metric control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_SystemMetric_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlSystemMetric), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontrolsystemmetric"" class=""wx-webapp-system-metric""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the message queue status control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_MessageQueueStatus_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlMessageQueueStatus), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontrolmessagequeuestatus"" class=""wx-webapp-message-queue-status""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the progress task control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_ProgressTask_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlProgressTask), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontrolprogresstask"" class=""wx-webapp-progress-task""*</div>");
+
+        /// <summary>
+        /// Test the get fragments function of the fragment manager for the chat control.
+        /// </summary>
+        [Fact]
+        public void GetFragments_Chat_ContentSecondary_TestPageA() => AssertGetFragments(typeof(TestApplication), typeof(FragmentControlChat), typeof(SectionContentSecondary), typeof(TestPageA), 1, @"<div id=""webexpress-webapp-test-testfragmentcontrolchat"" class=""wx-webapp-chat""*</div>");
+
+        /// <summary>
         /// Test the render function of the fragment manager.
         /// </summary>
         [Theory]
